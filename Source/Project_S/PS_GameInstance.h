f08710d8243c3f41891615aedbdcc3c940e435bb
pwdab <soundno07@naver.com>
2025-03-17 20:37:31 +0900

    Steam 연결 성공
    - Launch Game하면 Steam 오버레이 활성화 됨
    - Create Session 작동 안 함 > Steam API로 Create Session하도록 바꿔야 함

--- Source/Project_S/PS_GameInstance.h
+++ Source/Project_S/PS_GameInstance.h
@@ -4,6 +4,9 @@
 
 #include "Project_S.h"
 #include "Engine/GameInstance.h"
+#include "OnlineSubsystem.h"
+#include "Interfaces/OnlineSessionInterface.h"
+#include "OnlineSessionSettings.h"
 #include "PS_GameInstance.generated.h"
 
 /**
@@ -13,5 +16,28 @@ UCLASS()
 class PROJECT_S_API UPS_GameInstance : public UGameInstance
 {
 	GENERATED_BODY()
+
+	UPS_GameInstance();
 	
+public:
+	virtual void Init() override;
+	void SetMap(uint8 InMap);
+	void SetStage(uint8 InStage);
+	void SetLife(uint8 InLife);
+	void DeductLife();
+	void SetIsGameStart(bool NewBool);
+
+	uint8 GetMap() const;
+	uint8 GetStage() const;
+	uint8 GetLife() const;
+	bool IsGameStart() const;
+
+private:
+	uint8 Map;
+	uint8 Stage;
+	uint8 Life;
+	bool bIsGameStart;
+
+	IOnlineSessionPtr SessionInterface;
+	TSharedPtr<class FOnlineSessionSearch> SessionSearch;
 };
